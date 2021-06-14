b25664c2b8e20b71ed5b9c6a50a1fa373121f04c
Don Chaney <dac5197@live.com>
2021-06-14 14:28:08 -0400

    Add project files.

---
+++ WebApi/Handlers/QueryHandlers/GetProductListHandler.cs
@@ -0,0 +1,28 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using WebApi.Data;
+using WebApi.Models;
+using WebApi.Requests.QueryRequests;
+
+namespace WebApi.Handlers.QueryHandlers
+{
+    public class GetProductListHandler : IRequestHandler<GetProductListQuery, List<Product>>
+    {
+        private readonly ApplicationDbContext _context;
+
+        public GetProductListHandler(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<List<Product>> Handle(GetProductListQuery request, CancellationToken cancellationToken)
+        {
+            var output = await _context.Products.ToListAsync(cancellationToken: cancellationToken);
+            return output;
+        }
+    }
+}
